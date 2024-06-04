4194e8809b75c23b55c5e8ff32a67de4243c07e3
Lucas Duarte <lucas.duartemusic@gmail.com>
2024-06-04 17:50:43 -0300

    Adicionar arquivos de projeto.

---
+++ GeradorDeTablaturas/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace GeradorDeTablaturas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Apenas um protótipo por enquanto;
+
+            string[] standardTuning = Console.ReadLine().Split(' ');
+            for (int i = 0; i < standardTuning.Length; i++)
+            {
+                Console.Write($"\n{standardTuning[i]}|");
+                for (int j = 0; j < 100; j++)
+                {
+                    Console.Write("-");
+                }
+                Console.Write("|");
+            }
+            Console.WriteLine();
+        }
+    }
+}
