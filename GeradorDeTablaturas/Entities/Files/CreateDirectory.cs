45e704936a686d22e8c01000e3caddc4239018a8
Lucas Duarte <lucas.duartemusic@gmail.com>
2024-06-05 17:19:28 -0300

    Imprementado as classes 'CreateDirectory' e 'CreateFile' para salvar a tablatura posteriormente.

---
+++ GeradorDeTablaturas/Entities/Files/CreateDirectory.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeradorDeTablaturas.Entities.Files
+{
+    internal class CreateDirectory
+    {
+        private static string DefaultFolderName { get; set; } = "tablature";
+        private static string MyDocumentsPath { get; set; } = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+        public static void CreateDefaultFolder()
+        {
+            string fullPath = Path.Combine(MyDocumentsPath, FirstLetterToUpperCase(DefaultFolderName));
+            if (!Directory.Exists(fullPath))
+            {
+                Directory.CreateDirectory(fullPath);
+            }
+            else
+            {
+                Console.WriteLine("Pasta ja existente");
+            }
+        }
+
+        private static string FirstLetterToUpperCase(string text)
+        {
+            return CultureInfo.CurrentCulture.TextInfo.ToTitleCase(text);
+        }
+    }
+}
